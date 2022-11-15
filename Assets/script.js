1ad608ca702194d47c3b3c54233bf525d70fa5b5
LBissette <loganbissette@gmail.com>
2022-11-14 23:39:29 -0500

    created README; added function for color change; added function for local storage

--- Assets/script.js
+++ Assets/script.js
@@ -3,13 +3,15 @@
 // in the html.
 var currentDayEl = $('#currentDay');
 var saveBtnEl = $('.saveBtn');
-var textarea = $('.description');
+var textArea = $('.description');
 var timeBox = document.querySelector = $('.container-lg');
 var timeBlock = document.querySelector= $('.time-block');
 var hour = dayjs().hour
+
 var events = [];
 
 
+$(function(){
  // TODO: Add code to display the current date in the header of the page.
 function currentDay() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -22,63 +24,57 @@ setInterval(currentDay,1000);
 
 
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-  $(document).ready($(function () {
-  saveBtnEl.on('click', function(event) {
-    
-    event.preventdefault();
-    var eventText = eventInput.value.trim();
+  $('.saveBtn').each(function(event){
+    $(this).off().on("click",function(e) {
+      e.preventDefault();
+      var input= $(event.target).siblings('textarea').val()
+      var mainDiv = $(event.target).parent('div').attr('id')
+      localStorage.setItem(mainDiv, input)
+      renderInput()
+      })
+  })
+  
+    // // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+  function renderInput() {
+    var h9 = $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+    var h10 = $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+    var h11 = $('#hour-11 .description').val(localStorage.getItem('hour-11'))
+    var h12 = $('#hour-12 .description').val(localStorage.getItem('hour-12'))
+    var h13 = $('#hour-13 .description').val(localStorage.getItem('hour-13'))
+    var h14 = $('#hour-14 .description').val(localStorage.getItem('hour-14'))
+    var h15 = $('#hour-15 .description').val(localStorage.getItem('hour-15'))
+    var h16 = $('#hour-16 .description').val(localStorage.getItem('hour-16'))
+    var h17 = $('#hour-17 .description').val(localStorage.getItem('hour-17'))
+    window.localStorage.getItem(h9, h10, h11, h12, h13, h14, h15, h16, h17)
+  }
 
-    if (eventText === '') {
-      return;
-    }
 
-  });
 
   // // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
   
-  // function colorChange() {
-  //   $(timeBlock).each(function(){
-  //     var timeSlot = parseInt($(this).attr('id').split('-')[1]);
-  //     if (timeSlot = hour) {
-  //       timeBlock.classList.add('present')
-  //     } else if (timeSlot > hour) {
-  //       timeBlock.classList.add('future')
-  //     } else {
-  //       timeBlock.classList.add('past')
-  //     }
-  //   })
-  // }
-  
-  
-
-  // // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-  
-  function storeEvent() {
-    localStorage.setItem('event', JSON.stringify(events));
+  function colorChange() {
+    $(timeBlock).each(function(){
+      var timeSlot = parseInt($(this).attr('id').split('-')[1]);
+      if (timeSlot === hour) {
+        timeBlock.classList.add('present')
+      } else if (timeSlot > hour) {
+        timeBlock.classList.add('future')
+      } else {
+        timeBlock.classList.add('past')
+      }
+    })
   }
+  colorChange()
   
 
 
-
+  
   function init() {
     var storedEvent = JSON.parse(localStorage.getItem('event'));
     if (storedEvent !== null) {
       events = storedEvent;
     }
-
-   
-    // colorChange()
-
   }
 
-
-
-  storeEvent();
-  
-
-
-
- 
-
   init();
-}));
+});
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Work-Day-Scheduler
+
+![Screenshot](/Assets/Photos/Screenshot_20221114_112914.png "Screenshot")
+## Description
+This website was developed to function as a work planner. Each hour is able to hold three lines of text. Throughout the day the blocks will change on whether they have happened, are yet to happen, or currently happening. Saving your todos will store them locally so you can return to the page later.
+
+## Installation
+
+Visit the website at [https://lbissette.github.io/Work-Day-Scheduler/] or download the repository from my GitHub [https://github.com/LBissette?tab=repositories] and open the index.html to view
+
+## Credits
+
+Logan Bissette - Javascript
+Trinity Education - HTML & CSS
